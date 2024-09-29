a3d7bfe1c8cb8d445d6902d4b376dbbe5b5a67fc
LordCosine <trigonometrichero@gmail.com>
2024-09-29 02:00:29 -0700

    fileSystem loading info
    yeh

--- Scenes/Global/menu_system.gd
+++ Scenes/Global/menu_system.gd
@@ -16,15 +16,16 @@ func _process(delta: float) -> void:
 
 # Add a menu 
 func LoadMenu(fileName: String) -> bool:
-	var jsonFile := FileAccess.open(fileName, FileAccess.READ)
+	var jsonFile := FileAccess.open("res://GameData/DesignData/Design.MenuData.json", FileAccess.READ)
 	var jsonParser:JSON = JSON.new()
 	var jsonArrParser:JSON = JSON.new()
 	jsonParser.parse(jsonFile.get_as_text())
 	
+	print("data:", type_string(typeof(jsonParser.data)) )
 	for menuObj:Variant in jsonParser.data:
+		print("menuObj: ", type_string(typeof(menuObj)))
 		var subObj = jsonParser.data[menuObj]
-		#print(menuObj, " ", subObj)
-		
+		print("subObj: ", type_string(typeof(subObj)))
 		if typeof(subObj) == TYPE_ARRAY:
 			print("\t",menuObj, ":")
 			for element in subObj:
