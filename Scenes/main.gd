ae1c7f97c0c8567a05d68c555ec46d71e01f3323
nahrs <rayool@nahrs.dev>
2024-10-01 01:00:24 -0400

    removing test cases

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,14 +1,5 @@
 extends Node2D
 
-
-
-# this is different
-# this is also different
-
-# hello world this is attempt number 3 
-
-#I found god. he was dead in a toilet.
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DesignData.LoadDesignData(DesignData.CMenuItem.cFileName)
@@ -31,41 +22,7 @@ func _ready() -> void:
 	$MenuSystem.LoadMenu("MainMenu")
 	$MenuSystem.set_position(get_viewport_rect().size / 2)
 	
-	#################################################################
-	# testing buttons
-	
-	#var viewportX = get_viewport().get_visible_rect().size.x / 2 
-	#var viewportY = get_viewport().get_visible_rect().size.y / 2
-	#
-	#var index : int = 0
-	#var totalButtons : int = 3
-	#var buttonGroup := ButtonGroup.new()
-	#
-	#var button = Button.new()
-	#
-	#button.text = "1poop"
-	#
-	#var button2 = Button.new()
-	#button2.text = "po2op"
-	#
-	#var button3 = Button.new()
-	#button3.text = "poop3"
-	#
-	#print("viewportx: ", viewportX)
-	#print("viewporty: ", viewportY)
-	#print("button 1 x: ", button.size.x)
-	#print("button 1 posx: ", button.position.x)
-	#print("button 1 posy: ", button.position.y)
-	#
-	#add_child(button)
-	#add_child(button2)
-	#add_child(button3)
-	#
-	#button.set_position(Vector2(viewportX + ((index * button.size.x) - (totalButtons * button.size.x)/2), viewportY + (index * button.size.y)))
-	#index += 1
-	#button2.set_position(Vector2(viewportX + (index * button2.size.x) - (totalButtons * button2.size.x)/2 , viewportY + (index * button2.size.y)))
-	#index += 1
-	#button3.set_position(Vector2(viewportX + (index * button3.size.x) - (totalButtons * button3.size.x)/2, viewportY + (index * button3.size.y)))
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
