1eaec90ef9e6dcbe217ac28a24d0baebd446c6f6
LordCosine <trigonometrichero@gmail.com>
2024-10-01 22:14:53 -0700

    DesignData
    MkII
    Large overhaul to DesignData system.
    Now instead of requesting properties like GetString
    You instead request the data Definition which is now a class object
    where you can do sexy .m_propName to get your various pieces of data.
    Also TableLink has been reinvisioned as TableKey
    A table.
    and a key to look up data within it.
    
    So "MenuItem[MainMenu.GameSelect]"
    the table is MenuItem
    and the key is MainMenu.GameSelect.
    
    you can use MainMenu.Gameselect as a key to find a piece of data contained in MenuItem table.
    
    Also there have been some changes to the data format.
    Every design data table now has two new properties
    "table" : "NameOfYourDataType"
    "keys" : a table of key to data.
    
    Design data is now bound to a Gdscript class.

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -2,27 +2,41 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	DesignData.LoadDesignData(DesignData.CMenuItem.cFileName)
-	DesignData.LoadDesignData(DesignData.CMenu.cFileName)
-	var menuText := DesignData.GetString(DesignData.CMenuItem.cTableName, "MainMenu.Options", DesignData.CMenuItem.cFieldText )
-	var menuOptions := DesignData.GetArray(DesignData.CMenu.cTableName, "MainMenu", DesignData.CMenu.cTableLinkArrayMenuItems)
-	var header := DesignData.GetString(DesignData.CMenu.cTableName, "MainMenu", DesignData.CMenu.cFieldHeader)
 	
-	print("menuText: ", menuText)
+	DesignData.LoadTable(DesignData.CMenuDefinition.m_tableName)
+	DesignData.LoadTable(DesignData.CMenuItemDefinition.m_tableName)
 	
-	print("header: ", header)
-	for tableLink:Variant in menuOptions:
-		var def := DesignData.GetDefinitionByTableLink(tableLink)
-		print("\tText: ", def[DesignData.CMenuItem.cFieldText])
-		print("\tSubText: ", def[DesignData.CMenuItem.cFieldSubText])
-		print("\tSubMenu: ", def[DesignData.CMenuItem.cTableLinkSubMenu])
-		print("\tCallback: ", def[DesignData.CMenuItem.cFieldCallback], "\n")
-	
+	#PrintMenuData("MainMenu")
+	#PrintMenuData("OptionsMenu")
+	#PrintMenuData("GameSelectMenu")
 	
 	$MenuSystem.LoadMenu("MainMenu")
 	$MenuSystem.set_position(get_viewport_rect().size / 2)
 	
 
+func PrintMenuData(menuKey:StringName):
+	var menuDef:DesignData.CMenuDefinition = DesignData.GetDefinition(DesignData.CMenuDefinition.m_tableName, menuKey)
+	
+	print("table: ", menuDef.m_id.m_table)
+	print("key: ", menuDef.m_id.m_key)
+	print("header: ", menuDef.m_header)
+	
+	for tableKey in menuDef.m_menuItems:
+		var menuItemDef:DesignData.CMenuItemDefinition = DesignData.GetDefinitionByTableKey(tableKey)
+		if menuItemDef == null:
+			continue
+			
+		print("\ttable: ", menuItemDef.m_id.m_table)
+		print("\tkey: ", menuItemDef.m_id.m_key)
+		print("\ttext: ", menuItemDef.m_text)
+		print("\tsubText: ", menuItemDef.m_subText)
+		print("\tcallBack: ", menuItemDef.m_callBack)
+		print("\tcallBackParams: ", menuItemDef.m_callBackParams)
+		if menuItemDef.m_subMenu.m_table.length() == 0:
+			print("\tsubMenu: " )
+		else:
+			print("\tsubMenu: ", menuItemDef.m_subMenu.m_table, "[", menuItemDef.m_subMenu.m_key, "]" )
+		print("")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
